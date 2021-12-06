b37c32fee331ec6852874ed7d8786e46fd0021a4
= <=>
2021-12-06 17:13:47 +0700

    up

---
+++ B2.sql
@@ -0,0 +1,53 @@
+﻿Lệnh INSERT: Tạo một bản ghi.
+Lệnh UPDATE: Sửa đổi các bản ghi.
+Lệnh DELETE: Xóa các bản ghi.
+
+-INSERT INTO TABLE_NAME (column1, column2, column3,...columnN)  
+VALUES (value1, value2, value3,...valueN);
+-INSERT INTO TABLE_NAME VALUES (value1,value2,value3,...valueN);
+
+-UPDATE table_name
+SET column1 = value1, column2 = value2...., columnN = valueN
+WHERE [condition];
+-Nếu muốn sửa đổi tất cả các giá trị ADDRESS và SALARY trong bảng CUSTOMERS, 
+ UPDATE CUSTOMERS
+SET ADDRESS = 'Ha Noi', SALARY = 1000.00;
+
+-DELETE FROM table_name
+WHERE [condition];
+
+
+
+insert into khoa (makhoa,tenkhoa,dienthoai)
+values ('cnuo',N'Công nghệ thoonh tin ứng dụng','0511962962');
+
+insert into khoa 
+values ('mmnh',N'Công nghệ thông tin ứng dụng','0511962962');
+
+update khoa
+set dienthoai = '1111962962'
+
+update khoa
+set dienthoai = '0911962962'
+where makhoa='cnuo'
+
+delete from khoa 
+where makhoa='cnuo'
+
+insert into lop (malop,tenlop,khoahoc,hedaotao,NamNhapHoc,makhoa)
+values (N'dh01', N'Đồ họa 1', 1, N'Chính quy', 2018,N'cnuo');
+insert into lop (malop,tenlop,khoahoc,NamNhapHoc,makhoa)
+values (N'dh02', N'Đồ họa 1', 1, 2018,N'cnuo');
+
+insert into sinhvien    CAST(3.00 AS Numeric(5, 2))
+values ('sv1',N'Hoang',N'Kiệt','15/03/1986 12:00:00 AM',1,N'Phong Nha, Quảng Bình',N'dh01')
+
+insert into sinhvien 
+values ('sv1',N'Hoang',N'Kiệt',CAST(N'1986-03-15T00:00:00' AS SmallDateTime),1,N'Phong Nha, Quảng Bình',N'dh01')
+
+insert into sinhvien 
+values ('sv2',N'Hoàng',N'Kiet',CAST(N'1986-03-15T06:14:12' AS SmallDateTime),1,N'Phong Nha, Quảng Bình',N'dh01')
+
+
+
+
---
+++ B3.sql
@@ -0,0 +1,94 @@
+﻿--T-SQL :Transact Structure Query Language
+SELECT col
+FROM tb
+WHERE condition
+--c1
+select *
+from SINHVIEN
+--c2
+select masv,hodem,ten
+from SINHVIEN
+inner join LOP 
+on SINHVIEN.MALOP = LOP.MALOP
+where LOP.KHOAHOC = 1
+--c3
+--ORDER BY tên-cột ASC, ... xếp tăng dần, mặc định.
+--ORDER BY tên-cột DESC, ... xếp giảm dần.
+select masv,hodem,ten,ngaysinh,gioitinh
+from SINHVIEN
+where SINHVIEN.GIOITINH = 'False'
+order by ten,hodem
+--c4
+select SINHVIEN.MASV,hodem,ten,diem
+from DIEMTHI 
+inner join SINHVIEN 
+on DIEMTHI.MASV = SINHVIEN.MASV
+where DIEMTHI.LANTHI = 1
+--c5
+select SINHVIEN.masv,hodem,ten,tenlop
+from SINHVIEN
+inner join DIEMTHI 
+on SINHVIEN.MASV = DIEMTHI.MASV
+inner join LOP
+on SINHVIEN.MALOP = LOP.MALOP
+inner join HOCPHAN
+on DIEMTHI.MAHOCPHAN = HOCPHAN.MAHOCPHAN
+where DIEMTHI.LANTHI>1 and HOCPHAN.TENHOCPHAN = N'HQTCSDL SQL Server'
+--c6
+select malop as N'Mã Lớp', tenlop as N'Tên Lớp' 
+from LOP
+where MAKHOA = N'khmt'
+--c7
+select masv,hodem,ten,tenlop,namnhaphoc
+from SINHVIEN
+inner join LOP
+on SINHVIEN.MALOP = LOP.MALOP
+inner join KHOA
+on LOP.MAKHOA = KHOA.MAKHOA
+where KHOA.TENKHOA = N'Thương mại điện tử'
+--c8
+--1tc = 15tiet
+select TENHOCPHAN, SOTINCHI*15 as N'Số tiết'
+from HOCPHAN
+--c9
+-- in = nhiều or
+select SINHVIEN.masv, hodem, ten
+from SINHVIEN
+inner join DIEMTHI 
+on SINHVIEN.MASV = DIEMTHI.MASV
+where DIEMTHI.LANTHI>1 and DIEMTHI.MAHOCPHAN IN ('sql', 'tinvp')
+--c10
+select top(2) SINHVIEN.masv, hodem, ten, diem
+from SINHVIEN
+inner join DIEMTHI
+on SINHVIEN.MASV = DIEMTHI.MASV
+inner join HOCPHAN
+on DIEMTHI.MAHOCPHAN = HOCPHAN.MAHOCPHAN
+where DIEMTHI.LANTHI = 1 and HOCPHAN.TENHOCPHAN = N'HQTCSDL SQL Server'
+order by DIEMTHI.DIEM DESC
+--c11
+select SINHVIEN.masv, hodem, ten, diem
+from SINHVIEN
+inner join DIEMTHI
+on SINHVIEN.MASV = DIEMTHI.MASV
+inner join HOCPHAN
+on DIEMTHI.MAHOCPHAN = HOCPHAN.MAHOCPHAN
+where DIEMTHI.LANTHI = 1 and HOCPHAN.TENHOCPHAN = N'Tin học văn phòng' and DIEMTHI.DIEM >= 5 and DIEMTHI.DIEM <= 7
+--c12,13
+--c14
+select SINHVIEN.masv, hodem, ten, tenlop, tenhocphan, diem, tenkhoa
+from SINHVIEN
+inner join LOP on SINHVIEN.MALOP = LOP.MALOP
+inner join KHOA on LOP.MAKHOA = KHOA.MAKHOA
+inner join DIEMTHI on SINHVIEN.MASV = DIEMTHI.MASV
+inner join HOCPHAN on DIEMTHI.MAHOCPHAN = HOCPHAN.MAHOCPHAN
+where DIEMTHI.LANTHI = 1 and DIEMTHI.DIEM>=8 and DIEMTHI.DIEM<=9
+--c15
+SELECT SINHVIEN.masv, hodem, ten, ngaysinh, lanthi, diem
+INTO KQTinDC
+FROM SINHVIEN
+inner join DIEMTHI on SINHVIEN.MASV = DIEMTHI.MASV
+inner join HOCPHAN on DIEMTHI.MAHOCPHAN = HOCPHAN.MAHOCPHAN
+Where HOCPHAN.TENHOCPHAN = N'Tin học đại cương'
+
+
